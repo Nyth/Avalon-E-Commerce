d43ce8898d0618e12782af439034b0acf60f90f0
Nayem-Al-Tareq <nayemaltareq786@gmail.com>
2024-11-12 21:51:18 -0330

    bugfix

--- backend/controller/payment.js
+++ backend/controller/payment.js
@@ -9,9 +9,9 @@ router.post(
   catchAsyncErrors(async (req, res, next) => {
     const myPayment = await stripe.paymentIntents.create({
       amount: req.body.amount,
-      currency: "inr",
+      currency: "cad",
       metadata: {
-        company: "Becodemy",
+        company: "Nayem",
       },
     });
     res.status(200).json({
